d8c4b3ebf50d4c1ae480eb96058473d0138e8d8f
Mohamed Haitham <moham9d.404@gmail.com>
2026-09-15 08:14:55 +0300

    create routes of all pages with react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import VerifyOtp from "./pages/VerifyOtp";
+import ForgetPassword from "./pages/ForgetPassword";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import ProductDetails from "./pages/ProductDetails";
+import Orders from "./pages/Orders";
+import OrderDetails from "./pages/OrderDetails";
+import OrderSuccess from "./pages/OrderSuccess";
+import Wishlist from "./pages/Wishlist";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Profile from "./pages/Profile";
 const App = () => {
   return (
-    <div className='text-center text-3xl'>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/verity-otp" element={<VerifyOtp />} />
+        <Route path="/forget-password" element={<ForgetPassword />} />
+        {/* ---------------------------------------------------- */}
+        <Route index element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/shop/:id" element={<ProductDetails />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/orders/:id" element={<OrderDetails />} />
+        <Route path="/order-success/:id" element={<OrderSuccess />} />
+        <Route path="/wishlist" element={<Wishlist />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/profile" element={<Profile />} />
+        {/* ---------------------------------------------------- */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
